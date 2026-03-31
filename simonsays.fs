0999b4e1df5206634be7b787c69869072bb49725
9daemons <enzo.espcor@gmail.com>
2026-03-31 11:19:55 +0200

    Current status: Working correctly, ordered and refactored, no comments in the code. It just reproduces a new led every time does not concatenate

--- simonsays.fs
+++ simonsays.fs
@@ -2,7 +2,7 @@
 variable tick
 variable pushed-button
 variable last-led
-variable contador
+variable counter
 
 \constants
 \leds
@@ -11,10 +11,10 @@ variable contador
 4 constant greenb
 8 constant blueb
 \masks
-15 constant button-mask \ Bits 0,1,2,3 (1+2+4+8 = 15)
-240 constant button-clear \ Inversa para entradas (255 - 15 = 240)
-60 constant leds-mask    \ Bits 2,3,4,5 (4+8+16+32 = 60)
-195 constant leds-clear
+15 constant button-mask \(1+2+4+8 = 15)
+240 constant button-clear \(255 - 15 = 240)
+60 constant leds-mask    \(4+8+16+32 = 60)
+195 constant leds-clear \(255-60 = 195)
 \leds
 4 constant red
 8 constant yellow
@@ -31,14 +31,23 @@ variable contador
 : stop ( n -- ) 0 do loop ;
 : on  ( -- ) portb c@ 16 or portb c! ;
 : off ( -- ) portb c@ 239 and portb c! ;
-: tone ( time , tone -- ) 0 do on dup stop off dup stop loop drop off ;
+: tone ( duration tone -- )
+    0 do
+        on dup stop 
+        off dup stop 
+    loop drop 
+    off ;
 : note1 low 50 tone ;
 : note2 medium 100 tone ;
 : note3 high 150 tone ;
 : note4 veryhigh 200 tone ;
-: leds-off 0 portd io! ;
+: leds-off ( -- ) 
+    portd c@ leds-clear and portd c! ;
+: show-led ( color -- ) 
+    leds-off portd c@ or portd c! ;
 : buttons-input ddrB c@ button-clear and ddrB c! ;
 : leds-output ddrD c@ leds-mask or ddrD c! ;
+
 \important-functions
 : random-color
     tick @
@@ -56,16 +65,17 @@ variable contador
     leds-output
     buttons-input
     leds-off ;
-: leer-botones ( -- n )
+: read-buttons ( -- n )
     pinB c@ invert button-mask and ;
-: actualizar-leds ( n -- )
-    4 * \ Multiplica por 4 para alinear con los LEDs
+: update-leds ( n -- )
+    4 *
     leds-off
-    or                       \ Mezcla el nuevo estado
-    portd c! ;               \ Lo envía al hardware
-: capturar-boton ( -- )
+    or
+    portd c! ;
+: capture-button ( -- )
     begin 
-        leer-botones dup 0> 
+        read-buttons 
+        dup 0= if drop 0 else 1 then 
     until
     dup 1 = if drop 0 else
     dup 2 = if drop 1 else
@@ -73,34 +83,32 @@ variable contador
     drop 3 then then then
     pushed-button !
     begin 
-        leer-botones 0= 
+        read-buttons 0= 
     until
-    50 ms ;
-: comprobar-jugada ( -- flag )
+    20 ms ;
+: check-play ( -- flag )
     pushed-button @ tick @ = if 
-        \ --- ACERTÓ ---
-        contador @ 1 + contador !  \ Le suma 1 a la variable contador
-        0                          \ Dejamos un 0 en la pila: UNTIL repetirá el bucle
+        counter @ 1 + counter !
+        0
     else
-        \ --- FALLÓ ---
-        1                          \ Dejamos un 1 en la pila: UNTIL romperá el bucle
-    then 
-;
+        0 counter !
+        1
+    then ;
 
 : playgame
     configuration-restart
+    0 counter !
     begin
         tickstore
         1000 ms
-        random-color portD
-        50 ms
-        random-sound
+        random-color show-led random-sound
         1000 ms
-        leds-off
-        capturar-boton
-    key? until 
-    0 portD ;
-
+        leds-off 
+        capture-button
+        check-play
+        key? or    
+    until            
+    leds-off ;
 
 
 
