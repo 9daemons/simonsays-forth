26a0023d8bf09e7b76c1a87b972092779521e859
9daemons <enzo.espcor@gmail.com>
2026-04-02 11:11:18 +0200

    functional v1.0, needs to be refactored

--- simonsays.fs
+++ simonsays.fs
@@ -2,8 +2,9 @@
 variable tick
 variable pushed-button
 variable last-led
-variable counter
-
+variable score
+variable fail-flag
+create sequence 100 allot
 \ constants
 \ leds
 1 constant redb
@@ -20,14 +21,14 @@ variable counter
 8 constant yellow
 16 constant green
 32 constant blue
-\sound
+\ sound
 200 constant low
 150  constant medium
 100 constant high
 50  constant veryhigh
 
 \ basic-functions
-: tickstore $46 io@ 4 mod tick ! ;
+: randomNumber $46 io@ 4 mod tick ! ;
 : stop ( n -- ) 0 do loop ;
 : on  ( -- ) portb io@ 16 or portb c! ;
 : off ( -- ) portb io@ 239 and portb c! ;
@@ -45,10 +46,9 @@ variable counter
 : note2 medium tone ;
 : note3 high tone ;
 : note4 veryhigh tone ;
-: leds-off ( -- ) 
-    portd io@ leds-clear and portd c! ;
-: show-led ( color -- ) 
-    leds-off portd io@ or portd c! ;
+: errornote medium tone 100 ms low tone ;
+: leds-off ( -- ) portd io@ leds-clear and portd c! ;
+: show-led ( color -- ) leds-off portd io@ or portd c! ;
 : buttons-input ddrB io@ button-clear and ddrB c! ;
 : leds-output ddrD io@ leds-mask or ddrD c! ;
 
@@ -90,35 +90,46 @@ variable counter
         read-buttons 0= 
     until
     20 ms ;
-: check-play ( -- flag )
-    pushed-button @ tick @ = if 
-        counter @ 1 + counter !
-        0
-    else
-        0 counter !
-        1
-    then ;
-create sequence 100 allot
-variable fail-flag
-
+: new-level ( -- )
+    $46 io@ 4 mod
+    sequence score @ + c! ;
+: play-sequence ( -- )
+    score @ 1 + 0 do
+        sequence i + c@ tick !
+        random-color show-led random-sound
+        300 ms leds-off 200 ms
+    loop ;
+: check-sequence ( -- flag )
+    0 fail-flag !
+    score @ 1 + 0 do
+        capture-button
+        pushed-button @ 
+        sequence i + c@ = if
+        else
+            1 fail-flag !
+            leave
+        then
+    loop
+    fail-flag @ ;   
+: fail-announce
+    255 portd io!
+    errornote
+    300 ms
+    leds-off ;
 : playgame
     configuration-restart
-    0 counter !
+    0 score !
+    lcd-init
     begin
-        tickstore
-        500 ms
-        random-color show-led random-sound
-        500 ms
-        leds-off 
-        capture-button
-        check-play
-        key? or    
-    until            
-    leds-off ;
-
-
-
-
-
-
-
+        1000 ms
+        lcd-score
+        new-level
+        play-sequence
+        check-sequence 
+        dup 0= if 
+            score @ 1 + score !
+        then
+        key? or
+    until 
+    fail-announce
+    lcd-game-over;
